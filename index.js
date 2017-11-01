d5376ec73dba3ef37f5ffaa8488ba39a3cd04d16
arokalla <apresh.roakalla@appshark.com>
2017-11-01 15:16:34 +0530

    changed hello message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World change!')
+  response.send('Hello World change new!')
 })
 
 app.get('/company', function(request, response) {
