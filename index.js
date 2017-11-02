4fde4c686f68878e0f579e92ebd966da66993f2e
arokalla <apresh.roakalla@appshark.com>
2017-11-02 19:34:31 +0530

    modeified

--- index.js
+++ index.js
@@ -8,34 +8,46 @@ app.get('/', function(request, response) {
    response.writeHead(200, {'Content-Type': 'text/html'});
    response.write('<h1>  ** Welcome to Backer Media Lead Management! ** </h1>');
    response.end();
-})
+});
 
 app.get('/company', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'application/json');
   response.send(JSON.stringify({ "companyName": "Appshark" }));
-})
+});
 
 app.get('/user', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'application/json');
   response.send(JSON.stringify({ "userName": "Ram" }));
-})
+});
 
 
 app.get('/user2', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'application/json');
   response.send(JSON.stringify({ "userName": "Ram" }));
-})
+});
 
 app.get('/user3', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'application/json');
   response.send(JSON.stringify({ "userName": "Ram" }));
-})
+});
 
+
+app.get('/user4', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({ "userName": "Ram" }));
+});
+
+app.get('/user5', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({ "userName": "Ram" }));
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
