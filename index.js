9b2ad1f483599bc4abf9ba38e9cee572eaabf952
arokalla <apresh.roakalla@appshark.com>
2017-11-02 12:12:00 +0530

    'addedservice'

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World change new!')
+  response.send('Welcome to Lead Management')
 })
 
 app.get('/company', function(request, response) {
   response.status(200);
   response.setHeader('Content-Type', 'application/json');
   response.send(JSON.stringify({ "companyName": "Appshark" }));
+})
+
+app.get('/users', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({ "userName": "arokalla" }));
 })
 
 
