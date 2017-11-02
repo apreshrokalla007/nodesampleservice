bac4909be27654d58e82e27e4084806436d37fbd
arokalla <apresh.roakalla@appshark.com>
2017-11-02 16:15:32 +0530

    modeified

--- index.js
+++ index.js
@@ -14,7 +14,11 @@ app.get('/company', function(request, response) {
   response.send(JSON.stringify({ "companyName": "Appshark" }));
 })
 
-
+app.get('/user', function(request, response) {
+  response.status(200);
+  response.setHeader('Content-Type', 'application/json');
+  response.send(JSON.stringify({ "userName": "Ram" }));
+})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
