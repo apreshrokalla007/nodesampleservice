b6de73e306c39f5faded04521ae6b7da514eb259
apresh.rokalla@appshark.com <apresh.roakalla@appshark.com>
2017-11-06 16:01:05 +0530

    'Added-Sql-Script'

--- table.sql
+++ table.sql
@@ -16,3 +16,11 @@ TABLESPACE pg_default;
 
 ALTER TABLE public."user"
     OWNER to postgres;
+	
+-- ALTER TABLE public."user" DROP COLUMN phone;
+
+ALTER TABLE public."user"
+    ADD COLUMN phone integer;
+	
+ALTER TABLE public."user"
+    ALTER COLUMN phone TYPE bigint ;	
